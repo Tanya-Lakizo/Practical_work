acea0e2ebbc8ab5a89a7dc1c32c67e546d496f42
Tanya <tanya.lakizo001@gmail.com>
2021-11-19 11:05:02 +0300

    Deleted adv

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+
+adv.forEach(item => {
+    item.remove();
+});
